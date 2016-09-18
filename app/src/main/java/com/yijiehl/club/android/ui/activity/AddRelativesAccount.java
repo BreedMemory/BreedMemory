99a8d5d773ddc5948cd4d0625d92c9662e7b701f
ZhangYiDream <18733170760@163.com>
2016-09-18 18:16:18 +0800

    个人信息修改详细页面

--- app/src/main/java/com/yijiehl/club/android/ui/activity/AddRelativesAccount.java
+++ app/src/main/java/com/yijiehl/club/android/ui/activity/AddRelativesAccount.java
@@ -1,6 +1,6 @@
 /**
  * 项目名称：孕育迹忆 <br/>
- * 文件名称: PhoneNumChangeActivity.java <br/>
+ * 文件名称: AddRelativesAccount.java <br/>
  * Created by 张志新 on 2016/9/18.  <br/>
  */
 package com.yijiehl.club.android.ui.activity;/**
@@ -8,58 +8,45 @@ package com.yijiehl.club.android.ui.activity;/**
  */
 
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 
 import com.uuzz.android.util.ioc.annotation.ContentView;
-import com.uuzz.android.util.ioc.annotation.OnClick;
 import com.uuzz.android.util.ioc.annotation.ViewInject;
 import com.yijiehl.club.android.R;
 
 /**
  * 项目名称：孕育迹忆 <br/>
- * 类  名: PhoneNumChangeActivity <br/>
+ * 类  名: AddRelativesAccount <br/>
  * 类描述: <br/>
  * 实现的主要功能 <br/>
  * 版    本：1.0.0 <br/>
  * 修改时间：2016/9/18 <br/>
- *
  * @author 张志新 <br/>
  */
-@ContentView(R.layout.activity_phone_num_change)
-public class PhoneNumChangeActivity extends BmActivity {
+@ContentView(R.layout.activity_add_relatives_account)
+public class AddRelativesAccount extends BmActivity {
 
-    @ViewInject(R.id.et_phone_change)
-    private EditText editText;
-    @ViewInject(R.id.iv_cancel_phone)
-    private ImageView imageView;
+    @ViewInject(R.id.et_relatives_input)
+    private EditText relEditText;
+
+    @ViewInject(R.id.et_name_input)
+    private EditText nameEditText;
+
+    @ViewInject(R.id.et_phone_input)
+    private EditText phoneEditText;
 
     @Override
     protected String getHeadTitle() {
-        return getString(R.string.phone_num);
+        return getString(R.string.relaccount);
     }
-
-    // TODO: 2016/9/18 标题栏的取消和保存功能还没有实现;布局大小也是暂时的；地址，昵称类似问题需要解决
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!TextUtils.isEmpty(editText.getText())) {
-            imageView.setVisibility(View.VISIBLE);
-        } else {
-            imageView.setVisibility(View.GONE);
-        }
+
+        // TODO: 2016/9/18 点击保存记得判断手机号合法以及信息是否完全
     }
 
-    @OnClick(R.id.iv_cancel_phone)
-    private void clearPhoneNum() {
-        if (!TextUtils.isEmpty(editText.getText())) {
-            editText.getText().clear();
-        }
-    }
 
     /**
      * 描 述：检测电话号码是否合法<br/>
---
+++ app/src/main/java/com/yijiehl/club/android/ui/activity/SexChangeActivity.java
@@ -0,0 +1,80 @@
+/**
+ * 项目名称：孕育迹忆 <br/>
+ * 文件名称: SexChangeActivity.java <br/>
+ * Created by 张志新 on 2016/9/18.  <br/>
+ */
+package com.yijiehl.club.android.ui.activity;/**
+ * Created by asus on 2016/9/18.
+ */
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.uuzz.android.util.ioc.annotation.ContentView;
+import com.uuzz.android.util.ioc.annotation.OnClick;
+import com.uuzz.android.util.ioc.annotation.ViewInject;
+import com.yijiehl.club.android.R;
+
+/**
+ * 项目名称：孕育迹忆 <br/>
+ * 类  名: SexChangeActivity <br/>
+ * 类描述: <br/>
+ * 实现的主要功能 <br/>
+ * 版    本：1.0.0 <br/>
+ * 修改时间：2016/9/18 <br/>
+ * @author 张志新 <br/>
+ */
+@ContentView(R.layout.activity_sex_change)
+public class SexChangeActivity extends BmActivity {
+
+    @ViewInject(R.id.layout_choose_male)
+    private RelativeLayout layoutMale;
+
+    @ViewInject(R.id.layout_choose_female)
+    private RelativeLayout layoutFemale;
+
+    @ViewInject(R.id.iv_male_show)
+    private ImageView maleShow;
+
+    @ViewInject(R.id.iv_female_show)
+    private ImageView femaleShow;
+
+
+    @Override
+    protected String getHeadTitle() {
+        return getString(R.string.sex);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String sex=getIntent().getStringExtra("sex");
+        if(TextUtils.isEmpty(sex)||sex.equals("male")){
+            maleShow.setVisibility(View.VISIBLE);
+        }else{
+            femaleShow.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    @OnClick(R.id.layout_choose_male)
+    private void chooseMale(){
+        femaleShow.setVisibility(View.GONE);
+        maleShow.setVisibility(View.VISIBLE);
+        // TODO: 2016/9/18 需要返回性别
+        //setResult();
+        finish();
+    }
+
+    @OnClick(R.id.layout_choose_female)
+    private void chooseFemale(){
+        maleShow.setVisibility(View.GONE);
+        femaleShow.setVisibility(View.VISIBLE);
+        // TODO: 2016/9/18 需要返回性别
+        //setResult();
+        finish();
+    }
+}
